d47f0beace5a518abf8390cdd7c600ceb96b3a4b
CJ Foley <me@cjfoley.com>
2016-04-06 21:55:34 -0700

    Adding muttrc

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc screenrc tmux.conf psqlrc ansible.cfg"    # list of files/folders to symlink in homedir
+files="bashrc vimrc screenrc tmux.conf psqlrc ansible.cfg muttrc"    # list of files/folders to symlink in homedir
 
 ##########
 
